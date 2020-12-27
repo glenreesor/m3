978e6607a3672c8f74f8ebf34ef9ecf5532a3a74
Glen Reesor <glen@wiseoldtree.ca>
2020-12-27 12:33:48 -0700

    Use 4 spaces for indentation

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-          title: 'm3',
-      }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                    title: 'm3',
+            }),
     ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
