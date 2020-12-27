e893e366ca123d2551567997650b80d1ce6c2467
Glen Reesor <glen@wiseoldtree.ca>
2020-12-27 12:30:39 -0700

    Basic webpack + typescript config
    Taken from https://webpack.js.org/guides/typescript/
    
    Also had to add the following to tsconfig.json, so ts compiler would restrict
    itself to src/ folder
    
      "include": [
          "src/**/*",
      ],
      "exclude": [
        "node_modules"
      ]

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+          title: 'm3',
+      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
