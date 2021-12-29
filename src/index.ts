98293be38260d0043049dd6399ce359dec7aa0e6
Glen Reesor <glen@wiseoldtree.ca>
2021-12-29 14:24:16 -0700

    Disable browser pull to refresh behavior

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import * as m from 'mithril';
 import App from './components/App';
 
 m.mount(document.body, App);
+
+// Disable browser pull to refresh so dragging to scroll m3 content doesn't
+// trigger a refresh (thereby turfing any unsaved content)
+document.getElementsByTagName('body')[0].style.overscrollBehavior = 'contain';
