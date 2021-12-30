341b1a474eabb6073b3ce5bc2439d07ba3d7df38
Glen Reesor <glen@wiseoldtree.ca>
2021-12-29 18:03:12 -0700

    Revert canvas dimensions change since it was acting weirdly

--- src/components/App.ts
+++ src/components/App.ts
@@ -33,7 +33,7 @@ function App(): m.Component {
             width: window.innerWidth - 20,
 
             // TODO: Turn this into a not-hack
-            height: window.innerHeight - MENU_HEIGHT - state.ui.getCurrentFontSize() - 30,
+            height: window.innerHeight - MENU_HEIGHT - state.ui.getCurrentFontSize() - 35,
         };
     }
 
