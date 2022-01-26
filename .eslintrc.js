81805a0d9ac30df7321bb880b10b0251d929efbc
Matthew Bennett <mattsbennett@gmail.com>
2022-01-25 22:04:18 -0800

    Add missing eslint jest plugin extends rule seemingly required to use jest/globals.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
-        "jest/globals": true,
+        'jest/globals': true,
     },
     extends: [
         'airbnb-base',
         'plugin:import/typescript',
+        'plugin:jest/recommended',
         'plugin:jsdoc/recommended',
     ],
     parser: '@typescript-eslint/parser',
