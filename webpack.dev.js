697e78db0c88af0eda80bc9323cdb3cb1df672d6
Matthew Bennett <mattsbennett@gmail.com>
2022-01-25 23:19:41 -0800

    Load global scss, scss modules and webfonts with webpack, chunk scss modules and minify css in production. Also, refactor existing webpack config: Use url-loader instead of file-loader in production to inline assets where possible.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const sass = require('sass');
 const common = require('./webpack.common.js');
 
 module.exports = merge(
@@ -8,6 +9,80 @@ module.exports = merge(
         devtool: 'inline-source-map',
         devServer: {
             host: '0.0.0.0',
+        },
+        module: {
+            rules: [
+                // All global styles live in global.scss. Let these cascade.
+                {
+                    test: /global\.scss$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                implementation: sass,
+                            },
+                        },
+                    ],
+                },
+                // Load the rest of the styles as (s)css modules.
+                {
+                    test: /\.scss$/,
+                    exclude: [/node_modules/, /global\.scss$/],
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                                modules: true,
+                                importLoaders: 1,
+                                localIdentName: '[name]__[local]___[hash:base64:5]',
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                implementation: sass,
+                                sourceMap: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'file-loader',
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                disable: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.woff(2)?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/',
+                            },
+                        },
+                    ],
+                },
+            ],
         },
     },
 );
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,91 @@
 const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const sass = require('sass');
 const common = require('./webpack.common.js');
+
+const sassLoaderConfig = {
+    loader: 'sass-loader',
+    options: {
+        implementation: sass,
+    },
+};
 
 module.exports = merge(
     common,
     {
         mode: 'production',
+        module: {
+            rules: [
+                // All global styles live in global.scss. Let these cascade.
+                {
+                    test: /global\.scss$/,
+                    exclude: /node_modules/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        sassLoaderConfig,
+                    ],
+                },
+                // Load the rest of the styles as (s)css modules.
+                {
+                    test: /\.scss$/,
+                    exclude: [/node_modules/, /global\.scss$/],
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                importLoaders: 1,
+                                // Just use className hashes in production to
+                                // shorten/obfuscate classNames
+                                localIdentName: '[hash:base64:5]',
+                            },
+                        },
+                        'postcss-loader',
+                        sassLoaderConfig,
+                    ],
+                },
+                // Use url-loader instead of file-loader in production for
+                // assets to inline wherever possible (for performance, though
+                // not as debug-friendly).
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'url-loader',
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                disable: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.woff(2)?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'fonts/',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        optimization: {
+            minimizer: [
+                new CssMinimizerPlugin(),
+            ],
+        },
+        plugins: [
+            // Extracts and merges SCSS modules with other (global) SCSS into a
+            // single CSS output file.
+            new MiniCssExtractPlugin(),
+        ],
     },
 );
